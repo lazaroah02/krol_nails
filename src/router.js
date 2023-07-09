c1d4782daf7c583553c6341c10f181ffe47c95cb
lazaroah <lazaroah02@gmail.com>
2023-07-08 20:16:41 -0400

    add hash router to fix the problem with navigation

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import HomePage from './pages/HomePage.vue'
 import ContactPage from './pages/ContactPage.vue'
 import JobsPage from './pages/JobsPage.vue'
@@ -22,7 +22,7 @@ const routes = [
     },
 ]
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes, 
 })
 export default router
